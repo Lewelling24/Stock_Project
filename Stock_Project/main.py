8e5beb73cef53013361d9b43c90aaf07c51b5e20
Kane Lewelling <kanelewelling@gmail.com>
2024-06-07 16:08:21 -0400

    Add files via upload

--- Stock_Project/main.py
+++ Stock_Project/main.py
@@ -1,3 +1,11 @@
 from stock import getData
 
+#quicktest
 
+apiKey = ""
+symbol = "VOO"
+interval = "1min"
+
+stock_data = getData(apiKey,interval, symbol)
+
+print(stock_data.json())
--- Stock_Project/stock.py
+++ Stock_Project/stock.py
@@ -2,11 +2,11 @@ import requests
 
 #12 data api
 apiKey = ""
-symbol = "NVDA"
+symbol = "VOO"
 interval = "1min"
 
 
 def getData(apiKey, interval, symbol):
     response = requests.get("https://api.twelvedata.com/time_series?apikey="+apiKey+"&interval="+interval+"&symbol="+symbol
-                        +"&start_date=2024-05-31 19:25:00&end_date=2024-06-03 19:25:00&format=JSON")
+                        +"&start_date=2024-06-06 19:25:00&end_date=2024-06-07 19:25:00&format=JSON")
     return response
