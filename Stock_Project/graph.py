09b853a4c20c9e6b5382f20651693c7dd21ad96f
kane <kanelewelling@gmail.com>
2024-06-19 12:11:43 -0400

    Updates to main.py and stock.py

--- Stock_Project/graph.py
+++ Stock_Project/graph.py
@@ -1,9 +1,9 @@
 # adding graphing to the project
 import matplotlib.pyplot as plt
 
-x_axis = [1, 2, 3]
-y_axis = [9000, 1075, 11223]
-y_axis1 = [9000, 1075, 11223]
+x_axis = [1, 2, 3, 4]
+y_axis = [9000, 1075, 11223, 11223]
+y_axis1 = [6730, 1424, 23456, 10293]
 plt.plot(x_axis, y_axis, y_axis1)
 plt.title("Stock Price over time")
 plt.xlabel("Time")
