60220bc5a7e9284972ab3369741cc6774d21e5f0
Lewelling24 <kanelewelling@gmail.com>
2024-06-20 19:51:00 -0400

    Building stock data class for the practice

--- Stock_Project/stock.py
+++ Stock_Project/stock.py
@@ -1,6 +1,6 @@
 import requests
 
-# 12 data api
+# This class is set up to make use of 12 data api
 apiKey = ""
 stock_symbol = "VOO"
 time_interval = "1day"
@@ -12,3 +12,27 @@ def get_data(api_key, interval, symbol, data_format):
         "https://api.twelvedata.com/time_series?apikey=" + api_key + "&interval=" + interval + "&symbol=" + symbol
         + "&start_date=2024-06-06 19:25:00&end_date=2024-06-07 19:25:00&format=" + data_format)
     return response.json()
+
+
+class StockData:
+    apiKey = ""
+    stock_symbol = ""
+    time_interval = ""
+    request_data_format = ""
+
+    def set_api_key(self, api_key):
+        self.apiKey = api_key
+
+    def set_stock_symbol(self, stocksymbol):
+        self.stock_symbol = stocksymbol
+        
+    def set_time_interval(self, timeinterval):
+        self.time_interval = timeinterval
+        
+    def set_request_data_format(self, data_format):
+        self.request_data_format = data_format
+
+
+        
+
+
