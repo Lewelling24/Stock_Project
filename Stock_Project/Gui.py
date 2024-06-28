3fd72c3161399ea1fff3a5d74ba2aa24a1d1571e
Lewelling24 <kanelewelling@gmail.com>
2024-06-28 18:32:58 -0400

    Adding code for gui

--- Stock_Project/Gui.py
+++ Stock_Project/Gui.py
@@ -1,12 +1,12 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 
 
 def window():
     app = QApplication(sys.argv)
 
-    new_window = QWidget()
+    new_window = QPushButton("Hello")
     new_window.show()
 
     app.exec()
